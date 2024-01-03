88df7e2ecddefb9b04959b718daeda8adbe2bbf8
Rajel Fishman <rajel@Rajels-MBP.lan>
2024-01-03 21:15:43 +0200

    Reverse1

--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,36 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
+
 	public static void main (String[] args){
-		//// Put your code here
+
+		String originalString =  (args [0]);
+		String reverseString = "";
+		char char1;
+		int position;
+		int length;
+
+		for (int i = 0; i < originalString.length(); i ++){
+
+			char1 = originalString.charAt(i);
+
+			reverseString = char1 + reverseString;
+
+
+		}
+		System.out.println(reverseString);
+
+		if (originalString.length() % 2 == 0){
+			position = originalString.length()/2+1;
+			length = 1;
+
+
+		}
+		else {
+			position = originalString.length()/2;
+			length = 1;
+		}
+		System.out.println("The middle character is " + originalString.substring(position, position + length));
+
 	}
 }
