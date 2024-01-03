64892a1eed14e3247f27d7aec38301240f176101
Rajel Fishman <rajel@Rajels-MBP.lan>
2024-01-03 19:09:49 +0200

    divisors1

--- Divisors.java
+++ Divisors.java
@@ -3,7 +3,16 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		int b = 1;
+		int x = Integer.parseInt(args [0]);
+
+		for (int i = 1; i <= x; i++) {
+			if(x % i == 0){
+				
+				System.out.println(" " + i );
+
+			}
+
+		}
 		
 	}
 }
