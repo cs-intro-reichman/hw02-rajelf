e1c4edbff6b48c55a074f9a495e314e1de86b952
Rajel Fishman <rajel@Rajels-MacBook-Pro.local>
2024-01-04 10:04:05 +0200

    perfect1

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,29 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt( args[0] );
+		int sum = 0;
+		String divisors = (n+ " is a perfect number since " + n + " = " + "1");
+		
+		for (int i=2; i<=n; i++){
+			if(n%i==0 && i!=n){
+				sum += i;
+				divisors += " + " + i;
+			
+			}
+		}
+		if (sum  +  1 == n) {
+			System.out.println(divisors);
+				
+		}
+		else{
+			System.out.println(n + " is not a perfect number");
+		}
 	}
 }
+			
+
+
+
+	
+
