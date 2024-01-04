abb85160488a665f8f1f6fd27855080f97ff22b6
Rajel Fishman <rajel@Rajels-MacBook-Pro.local>
2024-01-04 15:10:48 +0200

    OneOfEach1

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,24 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		int n;
+		boolean boy= false;
+		boolean girl= false;
+		int counter = 0;
+
+		while(boy == false || girl == false){
+			n = (int) (Math.random()*(2));
+			if(n ==  0){
+				boy = true;
+				System.out.print("b ");
+			}
+			else {
+				girl = true;
+				System.out.print("g ");
+			}
+			counter++;
+		}
+		System.out.println();
+		System.out.println("You made it... and you now have " +counter+ " children.");
 	}
 }
